2c753608b11fb6774b663f70ac445f693ac10018
pro97 <pro97@DESKTOP-H6B7TMG>
2021-09-23 21:21:22 +0900

    User 추가

---
+++ src/pufit/quotation/model/vo/User.java
@@ -0,0 +1,20 @@
+package pufit.quotation.model.vo;
+
+public class User {
+	
+	public User() {}
+	
+	private String userId;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	private String userPwd;
+	
+
+}
